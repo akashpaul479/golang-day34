17c62c337ddf57a8ee53f133e20eb2d353673f8e
akashpaul479 <akashpaul4790@gmail.com>
2026-01-06 20:53:28 +0530

    main files

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"jwtinhttponlycookies/golang-day34/jwtcookieswithcrudapi"
+)
+
+func main() {
+	// jwtinhttponlycookies.JWTCookie()
+	// jwtinhttponlycookies.BothAccessAndRefreshToken()
+	jwtcookieswithcrudapi.Jwtwithdatabases()
+}
